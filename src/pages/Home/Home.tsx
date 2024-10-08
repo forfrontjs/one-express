4dc6a5f776330fa395bc9d761ef60b516619ec45
Aimona <aimonadunaeva@gmail.com>
2024-10-08 15:54:18 +0600

    first map commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { Calculator } from "./ui/Calculator/Calculator"
+import MapSection from "./ui/MapSection/MapSection"
 
 export const Home = () => {
   return (
     <>
         <Calculator/>
+        <MapSection/>
     </>
   )
 }
