c8e3c85ab552bccd07a188956d2ce029a429b917
Momunov Daniyar <dmomunov9@gmail.com>
2024-10-08 16:41:48 +0600

    tracking section finished

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { Calculator } from "./ui/Calculator/Calculator"
+import Tracking from "./ui/Tracking/Tracking"
 
 export const Home = () => {
   return (
     <>
         <Calculator/>
+        <Tracking/>
     </>
   )
 }
