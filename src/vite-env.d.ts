fd5fab8846dfaff69fad711873e3fbf21a9f66f2
Рамазан <you@example.com>
2024-10-09 14:00:31 +0600

    add env files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_IMAGE_API_URL: string; // URL для получения массива картинок
+  readonly VITE_APP_URL: string; // Ваш основной URL приложения
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
