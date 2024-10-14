59e24415084ce3e7b4ee66d6f399cfa0b5770741
TheTemirlan08 <timasajnazarov@gmail.com>
2024-10-14 14:28:05 +0600

    create profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Footer } from "./components/Footer/Footer"
-import { Header } from "./components/Header"
+
+import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home/Home"
 import './assets/styles/index.scss'
+import { Layout } from "./components/Layout/Layout"
+import Profile from './pages/Profile/Profile'; // Правильный импорт по умолчанию
 
 export const App =  () => {
 
   return (
     <>
-      <Header />
-      <Home />
-      <Footer/>
+     <Routes>
+        <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+        </Route>
+     </Routes>
     </>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,17 @@
 import { Calculator } from "./ui/Calculator/Calculator"
+import { Contact } from "./ui/Contacts/Contact"
+import { Hero } from "./ui/Hero/Hero"
+import { MapSection } from "./ui/MapSection/MapSection"
+import { Tracking } from "./ui/Tracking/Tracking"
 
 export const Home = () => {
   return (
     <>
+        <Hero/>
         <Calculator/>
+        <Tracking/>
+        <Contact/>
+        <MapSection/>
     </>
   )
 }
